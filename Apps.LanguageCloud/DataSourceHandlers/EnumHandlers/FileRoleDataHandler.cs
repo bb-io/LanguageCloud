a3ec6807ee9fea1f8ce17e446e990d0e8f9a0b2e
mathijs-bb <mathijs.sonnemans@blackbird.io>
2023-11-27 14:53:23 +0100

    Updated upload source file input

---
+++ Apps.LanguageCloud/DataSourceHandlers/EnumHandlers/FileRoleDataHandler.cs
@@ -0,0 +1,19 @@
+﻿using Blackbird.Applications.Sdk.Utils.Sdk.DataSourceHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.LanguageCloud.DataSourceHandlers.EnumHandlers
+{
+    public class FileRoleDataHandler : EnumDataHandler
+    {
+        protected override Dictionary<string, string> EnumValues => new()
+        {
+            {"translatable", "Translatable"},
+            {"reference", "Reference"},
+            {"unknown", "Unknown" }
+        };
+    }
+}
---
+++ Apps.LanguageCloud/DataSourceHandlers/EnumHandlers/FileTypeDataHandler.cs
@@ -0,0 +1,19 @@
+﻿using Blackbird.Applications.Sdk.Utils.Sdk.DataSourceHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.LanguageCloud.DataSourceHandlers.EnumHandlers
+{
+    public class FileTypeDataHandler : EnumDataHandler
+    {
+        protected override Dictionary<string, string> EnumValues => new()
+        {
+            {"native", "Native"},
+            {"bcm", "BCM"},
+            {"sdlxliff", "SDL XLIFF" }
+        };
+    }
+}
