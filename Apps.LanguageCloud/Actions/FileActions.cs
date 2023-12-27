d6fcc961858324a9781360e896f7e0cace3c3c11
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-12-27 04:10:44 +0200

    New files

--- Apps.LanguageCloud/Actions/FileActions.cs
+++ Apps.LanguageCloud/Actions/FileActions.cs
@@ -7,14 +7,22 @@ using Apps.LanguageCloud.Dtos;
 using Apps.LanguageCloud.Models.Files.Requests;
 using Blackbird.Applications.Sdk.Common.Actions;
 using Newtonsoft.Json;
-using File = Blackbird.Applications.Sdk.Common.Files.File;
 using System.Net.Mime;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
+using Blackbird.Applications.Sdk.Utils.Extensions.Files;
 
 namespace Apps.LanguageCloud.Actions;
 
 [ActionList]
 public class FileActions
 {
+    private readonly IFileManagementClient _fileManagementClient;
+
+    public FileActions(IFileManagementClient fileManagementClient)
+    {
+        _fileManagementClient = fileManagementClient;
+    }
+
     [Action("List project source files", Description = "List project source files")]
     public ListAllFilesResponse ListSourceFiles(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders, 
         [ActionParameter] ListSourceFilesRequest input)
@@ -67,7 +75,9 @@ public class FileActions
             type = input.FileType ?? "native",
             language = input.SourceLanguageCode
         }), ParameterType.RequestBody);
-        request.AddFile("file", input.File.Bytes, input.File.Name);
+
+        var fileBytes = _fileManagementClient.DownloadAsync(input.File).Result.GetByteData().Result;
+        request.AddFile("file", fileBytes, input.File.Name);
         var response = client.Execute<UploadFileResponse>(request).Data;
         return response;
     }
@@ -78,14 +88,15 @@ public class FileActions
     {
         var client = new LanguageCloudClient(authenticationCredentialsProviders);
         var request = new LanguageCloudRequest($"/files", Method.Post, authenticationCredentialsProviders);
-        request.AddFile("file", input.File.Bytes, input.File.Name);
+        var fileBytes = _fileManagementClient.DownloadAsync(input.File).Result.GetByteData().Result;
+        request.AddFile("file", fileBytes, input.File.Name);
         var importOperation = client.Execute<ZipFileStatusDto>(request).Data;
         var pollingResult = client.PollImportZipArchiveOperation(importOperation.Id, authenticationCredentialsProviders);
         return pollingResult;
     }
 
     [Action("Download target file", Description = "Download target file by id")]
-    public DownloadTargetFileResponse DownloadTargetFile(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders,
+    public async Task<DownloadTargetFileResponse> DownloadTargetFile(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders,
         [ActionParameter] DownloadFileRequest input)
     {
         var client = new LanguageCloudClient(authenticationCredentialsProviders);
@@ -98,14 +109,12 @@ public class FileActions
         var downloadRequest = new LanguageCloudRequest($"/projects/{input.ProjectId}/target-files/{input.FileId}/versions/{targetFile.LatestVersion.Id}/exports/{pollingResult.Id}/download", 
             Method.Get, authenticationCredentialsProviders);
         var fileData = client.Get(downloadRequest).RawBytes;
-            
+
+        using var stream = new MemoryStream(fileData);
+        var file = await _fileManagementClient.UploadAsync(stream, MediaTypeNames.Application.Octet, targetFile.Name);
         return new DownloadTargetFileResponse()
         {
-            File = new File(fileData)
-            {
-                Name = targetFile.Name,
-                ContentType = MediaTypeNames.Application.Octet
-            }
+            File = file
         };
     }
 
